c40ebcaacc2f9ea1e87a2192f75f32999d0c8094
Tom Larsen <larsenthomasj@gmail.com>
2020-01-17 14:58:29 +0000

    Add macro-level absolute/relative macros functions.  Closes #7, closes #6

--- lib/right_bar.dart
+++ lib/right_bar.dart
@@ -1,92 +1,103 @@
 import 'package:flutter/material.dart';
 import 'package:ryx_gui/app_state.dart';
 import 'package:ryx_gui/bloc_provider.dart';
+import 'package:ryx_gui/left_bar.dart';
 
 class RightBar extends StatelessWidget {
   Widget build(BuildContext context) {
     var state = BlocProvider.of<AppState>(context);
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: <Widget>[
-        RaisedButton(
-          child: Text(
-            "Rename Macro",
-            overflow: TextOverflow.ellipsis,
-          ),
-          onPressed: null,
-        ),
-        RaisedButton(
-          child: Text(
-            "Move Macro",
-            overflow: TextOverflow.ellipsis,
-          ),
-          onPressed: null,
-          materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
-        ),
-        RaisedButton(
-          child: Text(
-            "Make macro relative",
-            overflow: TextOverflow.ellipsis,
-          ),
-          onPressed: null,
-        ),
-        RaisedButton(
-          child: Text(
-            "Make macro absolute",
-            overflow: TextOverflow.ellipsis,
-          ),
-          onPressed: null,
-          materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
-        ),
-        RaisedButton(
-          child: Text(
-            "Extract selection to macro",
-            overflow: TextOverflow.ellipsis,
-          ),
-          onPressed: null,
-        ),
-        RaisedButton(
-          child: Text(
-            "Make selection relative",
-            overflow: TextOverflow.ellipsis,
-          ),
-          onPressed: null,
-          materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
-        ),
-        RaisedButton(
-          child: Text(
-            "Make selection absolute",
-            overflow: TextOverflow.ellipsis,
-          ),
-          onPressed: null,
-        ),
-        Expanded(
-          child: Column(
-            children: <Widget>[
-              Text(
-                "Where used in project:",
+    return StreamBuilder(
+      stream: state.currentDocument,
+      builder: (context, AsyncSnapshot<String> snapshot){
+        if (!snapshot.hasData && snapshot.data != ""){
+          return Container();
+        }
+        return Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            RaisedButton(
+              child: Text(
+                "Rename Macro",
                 overflow: TextOverflow.ellipsis,
               ),
-              Expanded(
-                child: StreamBuilder(
-                  stream: state.whereUsed,
-                  builder: (context, AsyncSnapshot<List<String>> snapshot){
-                    if (snapshot.hasData){
-                      return ListView.builder(
-                        itemCount: snapshot.data.length,
-                        itemBuilder: (context, index){
-                          return Text(snapshot.data[index]);
-                        },
-                      );
-                    }
-                    return Container();
-                  },
-                ),
+              onPressed: null,
+            ),
+            RaisedButton(
+              child: Text(
+                "Move Macro",
+                overflow: TextOverflow.ellipsis,
               ),
-            ],
-          ),
-        ),
-      ],
+              onPressed: null,
+              materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+            ),
+            ChangePathsButton(
+              child: Text(
+                "Make macro relative",
+                overflow: TextOverflow.ellipsis,
+              ),
+              busyMessage: "Making macro relative in project workflows...",
+              changePathsAction: ()=>state.makeMacroRelative(snapshot.data),
+            ),
+            ChangePathsButton(
+              child: Text(
+                "Make macro absolute",
+                overflow: TextOverflow.ellipsis,
+              ),
+              materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+              busyMessage: "Making macro absolute in project workflows...",
+              changePathsAction: ()=>state.makeMacroAbsolute(snapshot.data),
+            ),
+            RaisedButton(
+              child: Text(
+                "Extract selection to macro",
+                overflow: TextOverflow.ellipsis,
+              ),
+              onPressed: null,
+            ),
+            RaisedButton(
+              child: Text(
+                "Make selection relative",
+                overflow: TextOverflow.ellipsis,
+              ),
+              onPressed: null,
+              materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+            ),
+            RaisedButton(
+              child: Text(
+                "Make selection absolute",
+                overflow: TextOverflow.ellipsis,
+              ),
+              onPressed: null,
+            ),
+            Expanded(
+              child: Column(
+                children: <Widget>[
+                  Text(
+                    "Where used in project:",
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  Expanded(
+                    child: StreamBuilder(
+                      stream: state.whereUsed,
+                      builder: (context, AsyncSnapshot<List<String>> snapshot){
+                        if (snapshot.hasData){
+                          return ListView.builder(
+                            itemCount: snapshot.data.length,
+                            itemBuilder: (context, index){
+                              return Text(snapshot.data[index]);
+                            },
+                          );
+                        }
+                        return Container();
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        );
+      },
     );
   }
 }
